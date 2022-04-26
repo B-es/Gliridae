67384f91e3178febea81873c7de419eb4320e0fe
B-es <104147126+B-es@users.noreply.github.com>
2022-04-26 18:00:14 +0300

    Vot

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,44 @@
+//Скролл
+let last_position = 0;
+let ticking = false;
 
+
+function dosome(scroll_pos){
+    if(scroll_pos > 600)
+    document.querySelector('.background').style.backgroundColor = "red";
+    else
+    document.querySelector('.background').style.backgroundColor = "aquamarine";
+}
+
+
+window.addEventListener('scroll', function() {
+    last_position = this.window.scrollY;
+
+    if(!ticking){
+        this.window.requestAnimationFrame(function() {
+            dosome(last_position);
+            ticking = false;
+        })
+    }
+
+    ticking = true;
+})
+
+
+//Наведение на иконку
+function dosome2(){
+    if(!onmouseover)
+    document.querySelector('.ico').style.marginBottom = "400px";
+    else
+    document.querySelector('.ico').style.marginBottom = "50px";
+}
+    
+
+
+window.addEventListener('show', function() {
+    
+    ico.requestAnimationFrame(function(){
+        dosome2();
+    })
+    ticking = true;
+})
