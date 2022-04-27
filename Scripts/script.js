e9f4863d7fa05e6513eb4f91bc0a122853290bf1
B-es <104147126+B-es@users.noreply.github.com>
2022-04-27 19:51:38 +0300

    Cool things

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,54 +1,9 @@
-function test(){
-    alert("Что жмёшь-то, я и так никого тут не трогаю")
-}
-
-//Анимация при наведении мыши на иконку
-
-
-
-//Анимация скролла
-
-let last_position = 0;
-let ticking = false;
-
-
-//Наведение на иконку
-function dosome2(){
-    if(!onmouseover)
-    document.querySelector('.ico').style.marginBottom = "400px";
-    else
-    document.querySelector('.ico').style.marginBottom = "50px";
-}
-    
-
-
-window.addEventListener('show', function() {
-    
-    ico.requestAnimationFrame(function(){
-        dosome2();
-    })
-    ticking = true;
-})
-
-
-//Кнопки, что вечно мертвы
-
-function goUrl(){
-    let right = confirm("Хочешь посмотреть видик?..", 'Gliridae...');
-
-    if(right){
-        document.location.href ="https://youtu.be/D-SQqppuGvc"; 
-    }
-    else{
-        alert("Как хочешь...")
-    }
-}
-
-
-
 //Добавление событий после полной прогрузки DOM
+let n;
 
 window.addEventListener('DOMContentLoaded', function(){
+
+
 
     //Иконка
     let pic = document.getElementById('pic');
@@ -59,8 +14,124 @@ window.addEventListener('DOMContentLoaded', function(){
     let elem = document.getElementsByClassName('nav__link')
     console.log(elem[2]);
     for (let index = 0; index < elem.length; index++) {
-        elem[index].addEventListener('click', goUrl);
+        elem[index].addEventListener('click', () => confirmtion('Тут будет музычка, хочешь?..', "Да...", goUrl, "Ну как хочешь...", n));
         
     }
 
+    
+    
+    
+    document.getElementById('conf').addEventListener('click', closeco);
+    document.getElementById('closer2').addEventListener('click', closeco);
+    document.getElementById('closer').addEventListener('click', closeal);
+    document.getElementById('aler').addEventListener('click', closeal);
+    
 })
+
+
+//Новые всплывающие окна
+
+//Предупреждение
+function alertion(text){
+    elem = document.querySelector('#altx');
+    elem.textContent = text;
+    console.log(text);
+
+    aler = document.querySelector('.aler');
+    aler.style.display = 'block';
+
+    clos = document.querySelector('#closer');
+    clos.style.display = 'block';
+
+    aler.style.opacity = "1";
+    clos.style.opacity = '0.5';
+}
+
+//Закрытие окна
+function closeal(){
+    aler = document.querySelector('.aler');
+    clos = document.querySelector('#closer');
+    setTimeout(opa(clos), 500);
+    setTimeout(opa(aler), 500);
+    aler.addEventListener('transitionend', function(e){
+        dis(clos);
+        dis(aler);
+    })
+
+        clos.addEventListener('transitionend', function(e){
+            dis(clos);
+            dis(aler);
+        })
+}
+
+//Подтверждение
+function confirmtion(text, text_2, func, text_3, n){
+    document.addEventListener("click", event => n = event.target.id)
+    elem2 = document.querySelector('#btn_tx')
+    elem2.textContent = text_2;
+    console.log(text_2);
+
+    elem = document.querySelector('#conftx');
+    elem.textContent = text;
+    console.log(text);
+
+    conf = document.querySelector('.conf');
+    conf.style.display = 'block';
+
+    clos = document.querySelector('#closer2');
+    clos.style.display = 'block';
+
+    conf.style.opacity = '1';
+    clos.style.opacity = '0.5';
+
+    document.getElementById('btn_tx').addEventListener('click', func);
+    if(n !== undefined){
+        if(n !== 'btn_tx'){
+            alertion(text_3)
+        }
+    }
+    document.removeElementById('btn_tx').addEventListener('click', func);
+    document.removeEventListener("click", event => n = event.target.id)
+}
+
+
+//Закрытие окна
+function closeco(){
+    conf = document.querySelector('.conf');
+    clos = document.querySelector('#closer2');
+    setTimeout(opa(clos), 500);
+    setTimeout(opa(conf), 500);
+
+        clos.addEventListener('transitionend', function(e){
+            dis(clos);
+            dis(conf);
+        })
+
+        conf.addEventListener('transitionend', function(e){
+            dis(clos);
+            dis(conf);
+        })  
+       
+    return false;
+}
+
+//Плавное закрытие
+function opa(elem){
+    elem.style.opacity = '0';
+}
+    
+function dis(elem){
+    elem.style.display = 'none';
+}
+
+function test(){
+    alertion("Что жмёшь-то, я никого не трогаю");
+}
+
+
+//Кнопки, что вечно мертвы
+
+function goUrl(){
+    document.location.href ="https://youtu.be/D-SQqppuGvc";  
+}
+
